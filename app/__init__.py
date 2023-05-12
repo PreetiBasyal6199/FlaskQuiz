3e6fd8fdf6c5c560d694aa93e9b2102411becdd0
preeti <preetibasyal412@gmail.com>
2023-05-12 12:06:35 +0545

    added Marshmallow

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
+from flask_marshmallow import Marshmallow
 
 app = Flask('__name__')
 app.config.from_object('config.Config')
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 jwt = JWTManager(app)
 
 
