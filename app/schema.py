f3cd6e30d284310bf53b0f4055f9f39bc0739c63
preeti <preetibasyal412@gmail.com>
2023-05-12 15:37:37 +0545

    added schema for question Model

--- app/schema.py
+++ app/schema.py
@@ -1,5 +1,6 @@
 from app import ma
-from app.models import Category
+from marshmallow import Schema, fields, validates, ValidationError
+from app.models import Category, Question
 
 
 class CategorySchema(ma.Schema):
@@ -7,7 +8,46 @@ class CategorySchema(ma.Schema):
         model = Category
         fields = ("id", "name", "created_at")
 
+
 class CategoryUpdateSchema(ma.Schema):
     class Meta:
         model = Category
-        fields = ("name", )
+        fields = ("name",)
+
+
+class QuestionCreateSchema(ma.Schema):
+    question_text = fields.String(required=True)
+    category_id = fields.Integer(required=True)
+    answer = fields.String(required=True)
+    score = fields.Integer(required=True)
+
+    class Meta:
+        model = Question
+        fields = ("question_text", "category_id", "answer", "score")
+
+    @validates('category_id')
+    def validate_category_id(self, value):
+        if not Category.query.get(value):
+            raise ValidationError('Invalid category ID')
+
+
+class QuestionListSchema(ma.Schema):
+    class Meta:
+        model = Question
+        fields = ("id", "question_text", "category_id", "answer", "score", "created_at")
+
+
+class QuestionUpdateSchema(ma.Schema):
+    question_text = fields.String()
+    category_id = fields.Integer()
+    answer = fields.String()
+    score = fields.Integer()
+
+    # class Meta:
+    #     model = Question
+    #     fields = ("question_text", "category_id", "answer", "score")
+
+    @validates('category_id')
+    def validate_category_id(self, value):
+        if not Category.query.get(value):
+            raise ValidationError('Invalid category ID')
