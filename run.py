d4076e0034a80a1ce31b283d7a7d4c0e77b374a4
preeti <preetibasyal412@gmail.com>
2023-05-12 12:07:34 +0545

    added resources to url

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 from app import app, db
+from flask_restful import Api
+from app.routes.quiz import CategoryResource
 
+api = Api(app)
 if __name__ == '__main__':
     from app.routes.user import simple_page
     app.register_blueprint(simple_page)
+    api.add_resource(CategoryResource, '/', '/category/', '/category/<category_id>/')
     with app.app_context():
         db.create_all()
     app.run(debug=True)
