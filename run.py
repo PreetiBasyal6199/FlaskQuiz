96aa1547aa2b695fe3d32fd9046b1b653f6e6013
preeti <preetibasyal412@gmail.com>
2023-05-13 17:30:00 +0545

    urls path refactored

--- run.py
+++ run.py
@@ -1,15 +1,15 @@
 from app import app, db
 from flask_restful import Api
-from app.routes.quiz import CategoryResource, QuestionResource, CategoryRetrieveResource, QuestionDetailResource
-from app.routes.player import CategoryListResource, QuestionListResource, PlayQuizAPI
+from app.routes.admin import CategoryResource, QuestionResource, CategoryRetrieveResource, QuestionDetailResource
+from app.routes.play import CategoryListResource, QuestionListResource, PlayQuizAPI
 api = Api(app)
 if __name__ == '__main__':
     from app.routes.user import simple_page
     app.register_blueprint(simple_page)
-    api.add_resource(CategoryResource, '/cms/category/', '/cms/category/', '/cms/category/<category_id>/')
-    api.add_resource(CategoryRetrieveResource, '/cms/category-detail/<category_id>/')
-    api.add_resource(QuestionResource, '/', '/cms/question/', '/cms/question/<question_id>/')
-    api.add_resource(QuestionDetailResource, '/cms/question-detail/<question_id>/')
+    api.add_resource(CategoryResource, '/admin/category/', '/admin/category/', '/admin/category/<category_id>/')
+    api.add_resource(CategoryRetrieveResource, '/admin/category-detail/<category_id>/')
+    api.add_resource(QuestionResource, '/admin/question/', '/admin/question/', '/cms/question/<question_id>/')
+    api.add_resource(QuestionDetailResource, '/admin/question-detail/<question_id>/')
     api.add_resource(QuestionListResource, '/question/<category_id>/')
     api.add_resource(CategoryListResource, '/category/')
     api.add_resource(PlayQuizAPI, '/play-quiz/<category_id>/')
