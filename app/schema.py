b1493d20e4a674ad6902a2c2c6e948d60c3282c8
preeti <preetibasyal412@gmail.com>
2023-05-13 11:34:37 +0545

    added schema for UserQuiz

--- app/schema.py
+++ app/schema.py
@@ -1,6 +1,7 @@
 from app import ma
 from marshmallow import Schema, fields, validates, ValidationError
-from app.models import Category, Question
+from app.models import Category, Question, Quiz, UserAnswer, User
+from flask import request
 
 
 class CategorySchema(ma.Schema):
@@ -51,3 +52,52 @@ class QuestionUpdateSchema(ma.Schema):
     def validate_category_id(self, value):
         if not Category.query.get(value):
             raise ValidationError('Invalid category ID')
+
+
+class PlayQuizSchema(ma.Schema):
+    quiz_id = fields.Integer(required=True)
+    question_id = fields.Integer(required=True)
+    answer = fields.String(required=True)
+
+    @validates('quiz_id')
+    def validate_quiz_id(self, value):
+        current_user: User = self.context.get('current_user')
+        quiz_obj: Quiz = Quiz.query.get_or_404(value)
+        if quiz_obj.user_id != current_user:
+            raise ValidationError('Invalid quiz ID')
+
+    @validates('question_id')
+    def validate_question_id(self, value):
+        quiz_obj: Quiz = Quiz.query.get_or_404(self.context.get('quiz_id'))
+        question_obj: Question = Question.query.get_or_404(self.context.get('question_id'))
+        if question_obj not in quiz_obj.questions:
+            raise ValidationError('Invalid question ID')
+
+
+class ResponseSchema(Schema):
+    question_id = fields.Integer(required=True)
+    answer = fields.String(required=True)
+
+    @validates('question_id')
+    def validate_question_id(self, value, **kwargs):
+        quiz_id = request.view_args.get('quiz_id')
+        quiz_obj: Quiz = Quiz.query.get_or_404(quiz_id)
+        question_obj: Question = Question.query.get_or_404(value)
+        if question_obj not in quiz_obj.get_questions():
+            raise ValidationError('Invalid question ID')
+
+
+class QuizSchema(Schema):
+    answers = fields.Nested(ResponseSchema, many=True)
+
+    @validates('answers')
+    def validate_answers(self, answers, **kwargs):
+        quiz_id = request.view_args.get('quiz_id')
+        # Get all the unique question IDs from the list of answers
+        question_ids = set(answer['question_id'] for answer in answers)
+        # Get all the question IDs for the quiz
+        quiz_obj = Quiz.query.get_or_404(quiz_id)
+        quiz_question_ids = set(question.id for question in quiz_obj.get_questions())
+        # Check if the set of quiz question IDs is a subset of the set of answer question IDs
+        if quiz_question_ids != question_ids:
+            raise ValidationError('Quiz must include all questions.')
