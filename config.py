8de95b687b93a19de78591923ade1e90910103f8
preeti <preetibasyal412@gmail.com>
2023-05-13 17:20:15 +0545

    added postgres database configuration

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ import os
 
 class Config:
     SECRET_KEY = config('SECRET_KEY', default="TESTSECRETKEY")
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
+    SQLALCHEMY_DATABASE_URI = config('SQLALCHEMY_DATABASE_URI',
+                                     default='postgresql://postgres:postgres@localhost:5432/quiz')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = 'jwt-secret-key'
