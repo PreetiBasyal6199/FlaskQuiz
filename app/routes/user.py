afd45bca31740225483b67a5f695972012135ac9
preeti <preetibasyal412@gmail.com>
2023-05-13 13:15:01 +0545

    refactored user signup and login views

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,34 +1,34 @@
 from flask import jsonify, request, Blueprint, abort
-from werkzeug.security import generate_password_hash, check_password_hash
 from app.models import User
 from run import db
 from flask_jwt_extended import create_access_token
 from marshmallow import ValidationError
+from app.schema import UserCreateSchema, UserLoginSchema
+from werkzeug.security import generate_password_hash
 
 simple_page = Blueprint('simple_page', __name__)
 
 
 @simple_page.route('/signup/', methods=['POST'])
 def signup():
-    email = request.json.get('email')
-    password = request.json.get('password')
-    user = User.query.filter_by(email=email).first()
-    if user:
-        abort(400, "User with this email already exists")
-    user = User(email=email, password=generate_password_hash(password, method='SHA256'))
-    db.session.add(user)
+    try:
+        user_schema = UserCreateSchema().load(request.json)
+    except ValidationError as err:
+        return err.messages, 400
+    user_obj = User(email=user_schema['email'],
+                    password=generate_password_hash(user_schema['password'], method='SHA256'))
+    db.session.add(user_obj)
     db.session.commit()
     return {"message": "Signup successfully"}
 
 
 @simple_page.route("/login/", methods=['POST'])
 def login():
-    email = request.json.get('email')
-    password = request.json.get('password')
-    user = User.query.filter_by(email=email).first()
-    if user:
-        if check_password_hash(user.password, password):
-            access_token = create_access_token(identity=user.id)
-            return jsonify({"access_token": access_token}), 200
-        raise abort(400, "Incorrect Credentials")
-    raise abort(400, " User with this email does not exist")
+    try:
+        user = UserLoginSchema().load(request.json)
+        validated_data = UserLoginSchema().validate(user)
+        user_obj = User.query.filter_by(email=validated_data.get('email')).first()
+        access_token = create_access_token(identity=user_obj.id)
+        return jsonify({"access_token": access_token}), 200
+    except ValidationError as err:
+        return err.messages, 400
