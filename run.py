7582f58891621e2969ee21922aad562bde87dd7c
preeti <preetibasyal412@gmail.com>
2023-05-14 21:20:46 +0545

    refactored url config

--- run.py
+++ run.py
@@ -1,17 +1,16 @@
 from app import app, db
 from flask_restful import Api
 from app.routes.admin import CategoryResource, QuestionResource, CategoryRetrieveResource, QuestionDetailResource
-from app.routes.play import CategoryListResource, QuestionListResource, PlayQuizAPI
+from app.routes.play import QuestionListResource, PlayQuizAPI
 api = Api(app)
 if __name__ == '__main__':
     from app.routes.user import simple_page
     app.register_blueprint(simple_page)
-    api.add_resource(CategoryResource, '/admin/categories/', '/admin/category/', '/admin/category/<category_id>/')
+    api.add_resource(CategoryResource, '/categories/', '/admin/category/', '/admin/category/<category_id>/')
     api.add_resource(CategoryRetrieveResource, '/admin/category-detail/<category_id>/')
     api.add_resource(QuestionResource, '/admin/questions/', '/admin/question/', '/admin/question/<question_id>/')
     api.add_resource(QuestionDetailResource, '/admin/question-detail/<question_id>/')
     api.add_resource(QuestionListResource, '/questions/<category_id>/')
-    api.add_resource(CategoryListResource, '/categories/')
     api.add_resource(PlayQuizAPI, '/play-quiz/<category_id>/')
     with app.app_context():
         db.create_all()
