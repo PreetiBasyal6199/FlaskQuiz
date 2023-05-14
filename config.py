cd2e80596cd90b89b8ee27d63ac7cf28af1f2500
preeti <preetibasyal412@gmail.com>
2023-05-14 21:25:04 +0545

    added documentation for postgres database URI

--- config.py
+++ config.py
@@ -4,6 +4,12 @@ import os
 
 class Config:
     SECRET_KEY = config('SECRET_KEY', default="TESTSECRETKEY")
+    """
+    Database URI is taken from .env file
+    Format of database URI:
+    
+    'postgresql://{{user}}:{password}@{{host}}:{port}/{database_name}'
+    """
     SQLALCHEMY_DATABASE_URI = config('SQLALCHEMY_DATABASE_URI',
                                      default='postgresql://postgres:postgres@localhost:5432/quiz')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
